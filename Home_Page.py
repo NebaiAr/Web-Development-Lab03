1ae11198bb1c0134e60c9db57788c1921ecb20ca
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-15 14:11:21 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -22,9 +22,9 @@ st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
 1. **Nebai Araya's Portfolio**: This page showcases Nebai Araya's portfolio, including his education, professional experience, projects, skills, and activities.
-2. Nathan Wilson's Portfolio: This page showcases Nathan Wilson's portfolio, including his education, professional experience, projects, skills, and activities.
-3. Elden Ring Boss Finder: This page is an Elden Ring boss search tool that has the user input a boss name (or have one randomly selected), which it uses to display information about the boss they searched. This includes dropped items, which the user can click on to view information about.
-4. IN DEVELOPMENT: This page is currently in development and will be available soon.
+2. **Nathan Wilson's Portfolio**: This page showcases Nathan Wilson's portfolio, including his education, professional experience, projects, skills, and activities.
+3. **Elden Ring Boss Finder**: This page is an Elden Ring boss search tool that has the user input a boss name (or have one randomly selected), which it uses to display information about the boss they searched. This includes dropped items, which the user can click on to view information about.
+4. **IN DEVELOPMENT**: This page is currently in development and will be available soon.
 
 """)
 

d152748837c8f006968198b3580acd6389fdc022
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-15 00:13:03 -0500

    Update Boss_Finder.py

--- pages/Boss_Finder.py
+++ pages/Boss_Finder.py
@@ -1,6 +1,30 @@
+import requests as req
+import random
 import streamlit as st
-import Boss_Finder_Functions
 
+url = "https://eldenring.fanapis.com/api"
+
+def getBossData(bossName=None):
+    if bossName:
+        response = req.get(f"{baseURL}/bosses?name={bossName}")
+    else:
+        response = req.get(f"{baseURL}/bosses")
+        bosses = response.json().get("data", [])
+        bossName = random.choice(bosses)["name"]
+        response = req.get(f"{baseURL}/bosses?name={bossName}")
+    return response.json()
+
+def getRandomBoss():
+    response = req.get(f"{baseURL}/bosses")
+    bosses = response.json().get("data", [])
+    randomBossName = random.choice(bosses)["name"]
+    response = req.get(f"{baseURL}/bosses?name={randomBossName}")
+    return response.json()
+
+def getItemData(itemType, itemName):
+    response = req.get(f"{baseURL}/{itemType}?name={itemName}")
+    return response.json()
+#------------------------------------------------------------------------------#
 st.title("Elden Ring Boss Finder")
 
 # Input box for boss name

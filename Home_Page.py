c3a051e1c21e5284b243b2b74abbe8aeb853c25c
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-15 14:07:58 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -10,7 +10,7 @@ st.header("CS 1301")
 st.subheader("Team 53, Web Development - Section C")
 st.subheader("Nathan Wilson, Nebai Araya")
 
-st.image("Images/Elden_Ring2.jpg")
+st.image("Images/Elden_Ring2.jpg", caption="Flying Dragon Agheel from the game Elden Ring")
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
 #       1. **Page Name**: Description

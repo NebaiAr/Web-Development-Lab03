a8f99bd99d50fccd765280c4867d6523d2c4039c
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-15 13:54:02 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -10,6 +10,7 @@ st.header("CS 1301")
 st.subheader("Team 53, Web Development - Section C")
 st.subheader("Nathan Wilson, Nebai Araya")
 
+st.image("Images/Elden Ring.avif", width=200)
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
 #       1. **Page Name**: Description

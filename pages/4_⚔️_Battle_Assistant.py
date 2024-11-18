25b43626d71e07a28f3e035fc07991b4137853d5
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-18 22:09:21 +0000

    Added AI API Key Stuff

--- pages/4_⚔️_Battle_Assistant.py
+++ pages/4_⚔️_Battle_Assistant.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCKpE5_kShn2_GcaL-oztw6Jn9_P9eITcs"
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
+response = model.generate_content("Write a poem about how learning web development is fun!") #enter your prompt here!
+print(response.text) #dont forget to print your response!
 
 st.title("AI Battle Assistant")

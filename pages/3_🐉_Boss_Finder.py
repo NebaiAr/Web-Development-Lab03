b7a8b00f78c4f9b768ded81c58654bf3f9727f01
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-18 17:57:30 -0500

    Update 3_🐉_Boss_Finder.py

--- pages/3_🐉_Boss_Finder.py
+++ pages/3_🐉_Boss_Finder.py
@@ -17,28 +17,28 @@ def getBossData(bossName=None):
 
 #NEW
 def getRandomBoss():
-    response = req.get(f"{baseURL}/bosses?limit=1").json()
-    totalBosses = response.get("total")
+    # response = req.get(f"{baseURL}/bosses?limit=1").json()
+    # totalBosses = response.get("total")
     
-    # Calculate total number of pages based on limit (100 items per page)
-    limit = 100
-    totalPages = ((totalBosses + limit - 1) // limit) - 1
+    # # Calculate total number of pages based on limit (100 items per page)
+    # limit = 100
+    # totalPages = ((totalBosses + limit - 1) // limit) - 1
 
-    randomPage = random.randint(0, totalPages)
-    response = req.get(f"{baseURL}/bosses?limit={limit}&page={randomPage}").json()
+    # randomPage = random.randint(0, totalPages)
+    # response = req.get(f"{baseURL}/bosses?limit={limit}&page={randomPage}").json()
 
-    # Randomly select a boss from the selected page
-    bosses = response.get("data") 
-    randomBoss = random.choice(bosses)
-    bossName = randomBoss["name"]
+    # # Randomly select a boss from the selected page
+    # bosses = response.get("data") 
+    # randomBoss = random.choice(bosses)
+    # bossName = randomBoss["name"]
     
     response = req.get(f"{baseURL}/bosses?name={bossName}").json()
     return response
-    # response = req.get(f"{baseURL}/bosses?limit=100")
-    # bosses = response.json().get("data")
-    # randomBossName = random.choice(bosses)["name"]
-    # response = req.get(f"{baseURL}/bosses?name={randomBossName}")
-    # return response.json()
+    response = req.get(f"{baseURL}/bosses?limit=100")
+    bosses = response.json().get("data")
+    randomBossName = random.choice(bosses)["name"]
+    response = req.get(f"{baseURL}/bosses?name={randomBossName}")
+    return response.json()
 
 #NEW
 def getItemData(response):

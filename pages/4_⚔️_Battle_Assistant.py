11d296c2d2c015155b205511e46a828ce03b974d
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-18 16:13:33 -0500

    Update 4_⚔️_Battle_Assistant.py

--- pages/4_⚔️_Battle_Assistant.py
+++ pages/4_⚔️_Battle_Assistant.py
@@ -2,5 +2,9 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-st.title("AI Battle Assistant")
+model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
+response = model.generate_content("Write a poem about how learning web development is fun!") #enter your prompt here!
 
+
+st.title("AI Battle Assistant")
+st.write(response.text)

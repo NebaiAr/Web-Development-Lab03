7b20c9f3c78c5027b479e0473e524fd2e6ae5b52
Nebai Araya <137359933+NebaiAr@users.noreply.github.com>
2024-11-18 22:53:48 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -24,7 +24,7 @@ Welcome to our Streamlit Web Development Lab03 app! You can navigate between the
 1. **Nebai Araya's Portfolio**: This page showcases Nebai Araya's portfolio, including his education, professional experience, projects, skills, and activities.
 2. **Nathan Wilson's Portfolio**: This page showcases Nathan Wilson's portfolio, including his education, professional experience, projects, skills, and activities.
 3. **Elden Ring Boss Finder**: This page is an Elden Ring boss search tool that has the user input a boss name (or have one randomly selected), which it uses to display information about the boss they searched. This includes dropped items, which the user can click on to view information about.
-4. **IN DEVELOPMENT**: This page is currently in development and will be available soon.
+4. **Elden Ring Battle Assistant**: This page is an Elden Ring Battle Assistant that prompts the user to enter in a weapon and sorcery (or have them randomly selected), then generates a detailed explanation on how you can pair them together in battle. Once the report is finished, the user can ask follow-up questions about the combination. All powered by the Google Gemini API.
 
 """)
 
